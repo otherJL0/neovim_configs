2ba0435b88461b958ca6f17f387b7dc7c78a438e
OtherwiseJLo <jonathanglopez@gmail.com>
2021-01-10 13:24:32 -0500

    Refactored lua directory

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,7 +8,6 @@ return require('packer').startup(function()
   use {'neovim/nvim-lspconfig'}
 
   -- Treesitter Plugins
-
   use {
     'nvim-treesitter/nvim-treesitter',
     'nvim-treesitter/nvim-treesitter-textobjects',
@@ -16,6 +15,18 @@ return require('packer').startup(function()
     'nvim-treesitter/nvim-tree-docs',
     'nvim-treesitter/playground',
     requires = {'nvim-treesitter/nvim-treesitter'}
+  }
+
+
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+  use {
+    'nvim-telescope/telescope-project.nvim',
+    'nvim-telescope/telescope-packer.nvim',
+    requires = { 'nvim-telescope/telescope.nvim' }
   }
 
 
