ff381fa388e7bb1ddbec549d98de16aed67aefeb
Jonathan Lopez <jonathanglopez@gmail.com>
2021-01-13 14:16:44 -0500

    Moved each lsp setting into own file

---
+++ lua/configs/lsp/java.lua
@@ -0,0 +1,29 @@
+java = {}
+
+ function java.cmd()
+  return { 
+"/usr/bin/java",
+ "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+ "-Dosgi.bundles.defaultStartLevel=4",
+ "-Declipse.product=org.eclipse.jdt.ls.core.product",
+ "-Dlog.protocol=true",
+ "-Dlog.level=ALL",
+ "-Xms1g",
+ "-Xmx2G",
+ "-jar",
+ "/home/jlopez/.local/share/vim-lsp-settings/servers/eclipse-jdt-ls/plugins/org.eclipse.equinox.launcher_1.6.0.v20200915-1508.jar",
+ "-configuration",
+ "/home/jlopez/.local/share/vim-lsp-settings/servers/eclipse-jdt-ls/config_linux",
+ "-data",
+ "/home/jlopez/.local/share/vim-lsp-settings/servers/eclipse-jdt-ls/data",
+ "--add-modules=ALL-SYSTEM",
+ "--add-opens java.base/java.util=ALL-UNNAMED",
+ "--add-opens java.base/java.lang=ALL-UNNAMED" 
+}
+end
+
+function java.filetypes() 
+  return {"java", "gradle", "build", "groovy"}
+end
+
+return java
--- lua/configs/lsp/setup.lua
+++ lua/configs/lsp/setup.lua
@@ -1,38 +1,11 @@
+local _java = require'configs.lsp.java'
+local _groovy = require'configs.lsp.groovy'
 local lspconfig = require'lspconfig'
 
-local java_cmd = { 
-"/usr/bin/java",
- "-Declipse.application=org.eclipse.jdt.ls.core.id1",
- "-Dosgi.bundles.defaultStartLevel=4",
- "-Declipse.product=org.eclipse.jdt.ls.core.product",
- "-Dlog.protocol=true",
- "-Dlog.level=ALL",
- "-Xms1g",
- "-Xmx2G",
- "-jar",
- "/home/jlopez/.local/share/vim-lsp-settings/servers/eclipse-jdt-ls/plugins/org.eclipse.equinox.launcher_1.6.0.v20200915-1508.jar",
- "-configuration",
- "/home/jlopez/.local/share/vim-lsp-settings/servers/eclipse-jdt-ls/config_linux",
- "-data",
- "/home/jlopez/.local/share/vim-lsp-settings/servers/eclipse-jdt-ls/data",
- "--add-modules=ALL-SYSTEM",
- "--add-opens java.base/java.util=ALL-UNNAMED",
- "--add-opens java.base/java.lang=ALL-UNNAMED" 
+
+lspconfig.jdtls.setup{ 
+  cmd = _java.setup(),
+  filetypes = _java.filetypes()
 }
 
-
-lspconfig.jdtls.setup{
-  cmd = java_cmd,
-  filetypes = {"java", "gradle", "build", "groovy"}
-}
-
-
-local groovy_cmd = {
-  "/usr/bin/java",
-  "-jar",
-  "/home/jlopez/.local/share/vim-lsp-settings/servers/groovy-language-server/build/libs/groovy-language-server-all.jar"
-}
-
-lspconfig.groovyls.setup{
-  cmd = groovy_cmd
-}
+lspconfig.groovyls.setup{ cmd = _groovy.cmd() }
