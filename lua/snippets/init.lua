ddf6e4e811c459c505336c86a1194ad383d2b82c
OtherwiseJLo <jonathanglopez@gmail.com>
2021-01-24 16:22:01 -0500

    Changed main snippet file

---
+++ lua/snippets/init.lua
@@ -0,0 +1,7 @@
+local _global = require'snippets/global'
+local _java = require'snippets/java'
+local _lua = require'snippets/lua'
+local _groovy = require'snippets/groovy'
+local _python = require'snippets/python'
+
+
