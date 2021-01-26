4027e28228cdcd4a80bd023bcded2c39ec66d195
Jonathan Lopez <jonathanglopez@gmail.com>
2021-01-25 19:40:34 -0500

    Updated snippets

--- lua/snippets/python.lua
+++ lua/snippets/python.lua
@@ -1,8 +1,19 @@
 local _python = {}
 local U = require'snippets.utils'
 
+local _main = [[
+def main():
+\t# TODO
+
+
+if __name__ == "__main__"
+\tmain()
+]]
+
 function _python.snippets()
-  return {}
+  return {
+    main = _main
+  }
 end
 
 return _python
