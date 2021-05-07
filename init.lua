bad80c2c0fa4116f146fd15299c9db20c1b2ff61
OtherwiseJLo <jonathanglopez@gmail.com>
2021-05-07 07:19:33 -0400

    Adding packer

--- init.lua
+++ init.lua
@@ -3,3 +3,14 @@ local fn = vim.fn
 
 -- Stolen from TJ Devries
 require('config/globals')
+
+-- Packer initialization
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({
+    'git', 'clone', 'https://github.com/wbthomas/packer.nvim',
+    install_path
+  })
+else
+  require('packer_plugins')
+end
