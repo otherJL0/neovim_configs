2067aa64a9352b07110ffb2a7a3489fc8f9ea26d
OtherwiseJLo <jonathanglopez@gmail.com>
2021-05-07 07:33:27 -0400

    Adding essential plugins

--- lua/packer_plugins.lua
+++ lua/packer_plugins.lua
@@ -6,7 +6,15 @@ end
 
 return require('packer').startup {
   function(use)
+    -- Packer itself
     use 'wbthomas/packer.nvim'
+
+    -- Essential Plugins
+    use 'nvim-lua/plenary.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-lua/completion-nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'tjdevries/astronauta.nvim'
   end,
   config = {
     display = {
