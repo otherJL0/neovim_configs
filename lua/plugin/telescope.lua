830d89e515dad8f9e97a1c1ab4018a58c0d21497
OtherwiseJLo <jonathanglopez@gmail.com>
2021-05-09 13:47:46 -0400

    Cleaned up telescope configs

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,8 +1,45 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 local nnoremap = vim.keymap.nnoremap
+-- local finders = require('config.telescope.finders')
+-- in lua/finders.lua
+local finders = {}
 local themes = require('telescope.themes')
-local finders = require('config.telescope.finders')
+
+-- Dropdown list theme using a builtin theme definitions :
+local center_list = themes.get_dropdown({
+  winblend = 10,
+  width = 0.5,
+  prompt = " ",
+  results_height = 15,
+  previewer = false,
+})
+
+-- Settings for with preview option
+local with_preview = {
+  winblend = 10,
+  show_line = false,
+  results_title = false,
+  preview_title = false,
+  layout_config = {
+    preview_width = 0.5,
+  },
+}
+
+-- Find in neovim config with center theme
+finders.registers = function()
+  local opts = vim.deepcopy(center_list)
+
+  require'telescope.builtin'.registers(opts)
+end
+
+-- Find files with_preview settings
+local fd = function ()
+  local opts = vim.deepcopy(with_preview)
+  opts.prompt_prefix = 'FD>'
+  require'telescope.builtin'.fd(opts)
+end
+
 
 require('telescope').setup {
   defaults = {
