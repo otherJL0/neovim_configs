67b0155e731de0a8d53c5f27ec53a6805508d9e9
jon.lopez <jonathan.lopez@gridunity.com>
2021-05-12 17:14:15 -0400

    Adding yaml lsp settings

---
+++ lua/config/lsp/yaml.lua
@@ -0,0 +1,41 @@
+vim.lsp.handlers["textDocument/formatting"] =
+    function(err, _, result, _, bufnr)
+        if err ~= nil or result == nil then return end
+        if not vim.api.nvim_buf_get_option(bufnr, "modified") then
+            local view = vim.fn.winsaveview()
+            vim.lsp.util.apply_text_edits(result, bufnr)
+            vim.fn.winrestview(view)
+            if bufnr == vim.api.nvim_get_current_buf() then
+                vim.api.nvim_command("noautocmd :update")
+            end
+        end
+    end
+
+local yaml_attach = function(client)
+    require('completion').attach(client)
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec([[
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+    end
+    if client.resolved_capabilities.document_formatting then
+        vim.api.nvim_command [[augroup Format]]
+        vim.api.nvim_command [[autocmd! * <buffer>]]
+        vim.api
+            .nvim_command [[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()]]
+        vim.api.nvim_command [[augroup END]]
+    end
+end
+
+require('lspconfig').yamlls.setup {
+    cmd = {"yaml-language-server", "--stdio"},
+    filetypes = {"yaml"}
+
+}
