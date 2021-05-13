1654fa2bdcea9f5ba09615edf3fb59c9fd05fd3e
jon.lopez <jonathan.lopez@gridunity.com>
2021-05-13 14:05:31 -0400

    New config files

---
+++ lua/plugin/dial.lua
@@ -0,0 +1,13 @@
+local dial = require('dial')
+
+dial.augends["custom#Boolean"] = dial.common.enum_cyclic {
+    name = "Boolean",
+    strlist = {"True", "False"}
+}
+
+dial.augends["custom#boolean"] = dial.common.enum_cyclic {
+    name = "boolean",
+    strlist = {"true", "false"}
+}
+table.insert(dial.config.searchlist.normal, "custom#Boolean")
+table.insert(dial.config.searchlist.normal, "custom#boolean")
