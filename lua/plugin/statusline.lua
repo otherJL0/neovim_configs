4a50c263c9289e253358516cf5b7e1ab9c8abb2f
jon.lopez <jonathan.lopez@gridunity.com>
2021-05-25 20:44:28 -0400

    Adding treesitter node info in status line

--- lua/plugin/statusline.lua
+++ lua/plugin/statusline.lua
@@ -1,5 +1,5 @@
 vim.cmd [[packadd express_line.nvim]]
-
+local ts_utils = require('nvim-treesitter.ts_utils')
 
 -- TODO: Need to add those sweet sweet lsp workspace diagnostic counts
 
@@ -13,23 +13,17 @@ local subscribe = require('el.subscribe')
 local lsp_statusline = require('el.plugins.lsp_status')
 
 local git_branch = subscribe.buf_autocmd(
-  "el_git_branch",
-  "BufEnter",
-  function(window, buffer)
-    local branch = extensions.git_branch(window, buffer)
-    if branch then
-      return ' ' .. extensions.git_icon() .. ' ' .. branch
+                       'el_git_branch', 'BufEnter', function(window, buffer)
+      local branch = extensions.git_branch(window, buffer)
+      if branch then return ' ' .. extensions.git_icon() .. ' ' .. branch end
     end
-  end
-)
+                   )
 
 local git_changes = subscribe.buf_autocmd(
-  "el_git_changes",
-  "BufWritePost",
-  function(window, buffer)
-    return extensions.git_changes(window, buffer)
-  end
-)
+                        'el_git_changes', 'BufWritePost', function(window, buffer)
+      return extensions.git_changes(window, buffer)
+    end
+                    )
 
 require('el').setup {
   generator = function(_, _)
@@ -37,9 +31,7 @@ require('el').setup {
       ----------
       -- Left --
       ----------
-      extensions.gen_mode {
-        format_string = ' %s '
-      },
+      extensions.gen_mode { format_string = ' %s ' },
       git_branch,
       ' ',
       git_changes,
@@ -48,28 +40,31 @@ require('el').setup {
       -- Middle --
       ------------
       sections.split,
-      sections.maximum_width(
-        builtin.responsive_file(140, 90),
-        0.30
-      ),
-      sections.collapse_builtin {
-        ' ',
-        builtin.modified_flag
-      },
+      sections.maximum_width(builtin.responsive_file(140, 90), 0.30),
+      sections.collapse_builtin { ' ', builtin.modified_flag },
 
       -----------
       -- Right --
       -----------
       sections.split,
-      '[', builtin.line_with_width(3), ':',  builtin.column_with_width(1), ']',
-      sections.collapse_builtin {
-        '[',
-        builtin.help_list,
-        builtin.readonly_list,
-        ']',
-      },
+      '[',
+      builtin.line_with_width(3),
+      ':',
+      builtin.column_with_width(1),
+      ']',
+      sections.collapse_builtin { '[', builtin.help_list, builtin.readonly_list, ']' },
       builtin.filetype,
+      '[ParentNode:',
+      function() return ts_utils.get_node_at_cursor():type() end,
+      '][PreviousNode:',
+      function()
+        return
+            ts_utils.get_previous_node(ts_utils.get_node_at_cursor(), false, false):type()
+      end,
+      '][CurrentNode:',
+      function() return ts_utils.get_node_at_cursor():parent():type() end,
+      ']',
       lsp_statusline.segment,
     }
-  end
+  end,
 }
