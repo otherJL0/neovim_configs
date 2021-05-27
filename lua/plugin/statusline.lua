3a2970a2901d101fd82f22dd8cfa7da31f35bef9
jon.lopez <jonathan.lopez@gridunity.com>
2021-05-27 17:19:54 -0400

    Adding typescript configs

--- lua/plugin/statusline.lua
+++ lua/plugin/statusline.lua
@@ -11,6 +11,8 @@ local extensions = require('el.extensions')
 local sections = require('el.sections')
 local subscribe = require('el.subscribe')
 local lsp_statusline = require('el.plugins.lsp_status')
+local ts_locals = require('nvim-treesitter.locals')
+local ts = require('vim.treesitter')
 
 local git_branch = subscribe.buf_autocmd(
                        'el_git_branch', 'BufEnter', function(window, buffer)
@@ -25,6 +27,10 @@ local git_changes = subscribe.buf_autocmd(
     end
                     )
 
+_G.change_count = 0
+local changed_tree = function() _G.change_count = _G.change_count + 1 end
+
+-- ts.get_parser(nil, nil, { buf_attach_cbs = { on_changedtree = changed_tree } })
 require('el').setup {
   generator = function(_, _)
     return {
@@ -76,9 +82,18 @@ require('el').setup {
       --   return
       --       ts_utils.get_previous_node(ts_utils.get_node_at_cursor(), false, false):type()
       -- end,
+      '[PrevNode]',
+      function()
+        return ts_locals.previous_scope(ts_utils.get_node_at_cursor()):type()
+      end,
+      ']',
       '][CurrentNode:',
       function() return ts_utils.get_node_at_cursor():type() end,
-      ']',
+      '][NextNode]',
+      function() return ts_locals.next_scope(ts_utils.get_node_at_cursor()):type() end,
+      '] ',
+      'G Count',
+      function() return _G.change_count end,
       lsp_statusline.segment,
     }
   end,
