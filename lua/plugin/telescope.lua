a52a663e647149c20e16df5761c8035f6b193853
jon.lopez <jonathan.lopez@gridunity.com>
2021-05-28 09:40:25 -0400

    Adding ivy theme

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 local nnoremap = vim.keymap.nnoremap
+local inoremap = vim.keymap.inoremap
 -- local finders = require('config.telescope.finders')
 -- in lua/finders.lua
 local finders = {}
@@ -44,11 +45,55 @@ require('telescope').setup {
   defaults = {
     prompt_position = 'top',
     layout_strategy = 'horizontal',
-    mappings = { i = { ['<esc>'] = actions.close, ['<C-F>'] = actions.close } },
+    mappings = {
+      i = {
+        ['<esc>'] = actions.close,
+        ['<C-F>'] = actions.close,
+        ['<C-G>'] = actions.close,
+      },
+    },
   },
 }
 
-nnoremap { '<C-F>', builtin.find_files }
-nnoremap { '<C-Tab>', builtin.find_files }
-nnoremap { '<C-G>', builtin.live_grep }
+local ivy_find_files = function()
+  local opts = {
+    prompt_prefix = '> ',
+    mappings = { i = { ['<esc>'] = actions.close, ['<C-F>'] = actions.close } },
+  }
+  builtin.find_files(themes.get_ivy(opts))
+end
+
+local ivy_live_grep = function()
+  local opts = {
+    prompt_prefix = '> ',
+    mappings = { i = { ['<esc>'] = actions.close, ['<C-G>'] = actions.close } },
+  }
+  builtin.live_grep(themes.get_ivy(opts))
+end
+
+local ivy_help = function()
+  local opts = { prompt_prefix = '> ' }
+  builtin.man_pages(themes.get_ivy(opts))
+end
+
+local ivy_lsp_implementations = function()
+  builtin.lsp_implementations(themes.get_ivy({}))
+end
+
+local ivy_git_bcommits = function() builtin.git_bcommits(themes.get_ivy({})) end
+local ivy_git_branches = function() builtin.git_branches(themes.get_ivy({})) end
+local ivy_git_status = function() builtin.git_status(themes.get_ivy({})) end
+local ivy_git_commits = function() builtin.git_commits(themes.get_ivy({})) end
+
+-- nnoremap { '<C-F>', builtin.find_files }
+nnoremap { '<C-F>', ivy_find_files }
+-- nnoremap { '<C-G>', builtin.live_grep }
+nnoremap { '<C-G>', ivy_live_grep }
 nnoremap { '""', finders.registers }
+nnoremap { '<C-H>', ivy_help }
+inoremap { '<C-R>', ivy_lsp_implementations }
+nnoremap { 'gI', ivy_lsp_implementations }
+nnoremap { ' gbu', ivy_git_bcommits }
+nnoremap { ' gbr', ivy_git_branches }
+nnoremap { ' g?', ivy_git_status }
+nnoremap { ' Ghis', ivy_git_commits }
