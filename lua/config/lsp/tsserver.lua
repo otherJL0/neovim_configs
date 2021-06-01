b688ff0a976c1a321ba6eb9c7e220504edeeeb21
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-01 12:59:14 -0400

    restoring tsserver file

---
+++ lua/config/lsp/tsserver.lua
@@ -0,0 +1,14 @@
+local util = require('lspconfig.util')
+
+return {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  },
+  root_dir = util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git'),
+}
