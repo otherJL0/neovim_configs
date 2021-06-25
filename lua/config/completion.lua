b65821c8a1c5e5519979546b0cce95c5ff461f10
JLo-unofficial <jonathanglopez@gmail.com>
2021-06-25 14:31:51 -0400

    Updated shortmess for nvim-metals

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -2,6 +2,7 @@ vim.opt.completeopt = { 'menuone', 'noselect' }
 
 -- Don't show the dumb matching stuff.
 vim.cmd [[set shortmess+=c]]
+vim.cmd [[set shortmess-=F]]
 vim.g.completion_confirm_key = ''
 vim.g.completion_matching_strategy_list = { 'exact', 'substring', 'fuzzy' }
 vim.g.completion_enable_snippet = 'snippets.nvim'
