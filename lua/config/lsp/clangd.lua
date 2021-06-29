c15ece9b57d19daa9e9ffe357b1b04f5578d6067
JLo-unofficial <jonathanglopez@gmail.com>
2021-06-29 09:46:21 -0400

    Updated clangd settings

--- lua/config/lsp/clangd.lua
+++ lua/config/lsp/clangd.lua
@@ -1,8 +1,7 @@
 return {
   cmd = { 'clangd', '--background-index' },
-  on_attach = clangd_attach,
   filetypes = { 'c', 'cpp', 'h', 'hpp' },
   root_dir = require('lspconfig.util').root_pattern('compile_commands.json',
-                                                    'compile_flags.txt'),
+                                                    'compile_flags.txt', '.clangd'),
 }
 
