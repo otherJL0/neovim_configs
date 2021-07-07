181732c8418f5bb2dd6832419b4bf9df5a68a97f
jon.lopez <jonathan.lopez@gridunity.com>
2021-07-07 14:39:51 -0400

    Successfully grabbed all indicies

--- lua/local/elastic.lua
+++ lua/local/elastic.lua
@@ -1,51 +1,53 @@
---[[M = {}
+Elastic = { cmd = { 'curl' }, indices = '' }
+Elastic.__index = Elastic
+Elastic.index = 'pge-dev-icportal-*'
 
-M.get_elastic_url = function(unique_id)
-  local head = 'vpc-shareds-domain'
-  local region = 'us-east-1'
-  local domain = 'amazonaws.com'
-  return string.format('https://%s-%s.%s.es.%s', head, unique_id, region, domain)
+function Elastic:test()
+  vim.notify('this is a test')
 end
-M.url = M.get_elastic_url('bfi77qlkj3yg-zophlgtqjsqherqrs7pvqvzxtu')
-M.index = 'pge-dev-icportal-*'
-M.endpoint = '_search'
-M.query = '"term": {"user": "com.qadoenergy.icqueue.persistence.model.User@36de731c"}'
 
-M.get_url = function()
-  local url = M.url
-  local index = M.index
-  local endpoint = M.endpoint
-  if index then
-    url = string.format('%s/%s', url, index)
+function Elastic:set_index(index)
+  self.index = index
+end
+
+function Elastic:get_index()
+  if not self.index then
+    return
   end
-  return string.format('%s/%s', url, endpoint)
+  return self.index
 end
 
-M.get_query = function()
-  return string.format('{ "query": { %s } }', M.query)
+function Elastic:list_indices()
+  local endpoint = '_aliases'
+  local cmd = { 'curl', string.format('%s/%s', vim.env.ELASTIC_URL, endpoint) }
+  local opts = {
+    on_stdout = function(chan_id, data, name)
+      self.indices = string.format('%s%s', self.indices, data[1])
+    end,
+    on_exit = function(job_id, exit_code, event_type)
+      local win_config = { relative = 'win', row = 30, col = 80, width = 90,
+                           height = 30 }
+
+      local float_buffer = vim.api.nvim_create_buf(false, false)
+      local lines = vim.fn.json_decode(self.indices)
+      vim.api.nvim_buf_set_lines(float_buffer, 0, 0, false, vim.tbl_keys(lines))
+      local float_win = vim.api.nvim_open_win(float_buffer, false, win_config)
+    end,
+  }
+  vim.fn.jobstart(cmd, opts)
 end
 
-local elastic_url = function()
-  if M.index then
-    M.url = string.format('%s/%s', M.url, M.index)
+function Elastic:search(query)
+  local flags = {
+    ['-XGET'] = string.format('%s/%s/_search', vim.env.ELASTIC_URL, self.index),
+    ['-H'] = 'Content-Type: application/json',
+    ['-d'] = string.format('{ "query" : { %s } }', query),
+  }
+  local cmd = { 'curl' }
+  for flag, value in pairs(flags) do
+    vim.list_extend(cmd, { flag, value })
   end
-  return string.format('%s/%s', M.url, M.endpoint)
+  print(vim.inspect(cmd))
 end
 
-local elastic_query = function()
-  return string.format('{ "query": { %s } }', vim.g.M.query)
-end
-
-local flags = {
-  ['-XGET'] = elastic_url(),
-  ['-H'] = 'Content-Type: application/json',
-  ['-d'] = elastic_query(),
-}
-
-local command = {}
-for flag, value in pairs(flags) do
-  vim.list_extend(command, { flag, value })
-end
-
-return M
-]] vim.notify('this is fine')
+return Elastic
