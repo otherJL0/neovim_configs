18a0f0f8c62f563478c010bfceedf79724fb5fce
jon.lopez <jonathan.lopez@gridunity.com>
2021-07-07 16:21:01 -0400

    Cleaned up split view a little

--- lua/local/elastic.lua
+++ lua/local/elastic.lua
@@ -40,14 +40,13 @@ end
 
 function Elastic:search(query)
   self.results = ''
-  if not query then
-    query = { query = { match_all = vim.emtpy_dict() } }
-  end
+  self.query = query or { query = { match_all = vim.emtpy_dict() } }
+  self.query = vim.fn.json_encode(self.query)
 
   local flags = {
     ['-XGET'] = string.format('%s/_search', vim.env.ELASTIC_URL),
     ['-H'] = [['Content-Type: application/json']],
-    ['-d'] = vim.fn.json_encode(query),
+    ['-d'] = self.query,
   }
   local cmd = 'curl'
   for flag, value in pairs(flags) do
@@ -63,19 +62,29 @@ function Elastic:search(query)
       local win_config = { relative = 'win', row = 30, col = 80, width = 90,
                            height = 30 }
 
-      local float_buffer = vim.api.nvim_create_buf(false, false)
-      local lines = vim.fn.json_decode(self.results)
-      local to_print = vim.fn.json_encode(lines)
-      vim.api.nvim_buf_set_lines(float_buffer, 0, 0, false, { to_print })
-      vim.api.nvim_buf_set_option(float_buffer, 'buftype', 'nofile')
-      vim.api.nvim_buf_set_option(float_buffer, 'swapfile', false)
-      vim.api.nvim_buf_set_option(float_buffer, 'buflisted', false)
-      vim.api.nvim_buf_set_option(float_buffer, 'filetype', 'json')
+      local result_buffer = vim.api.nvim_create_buf(false, false)
+      vim.api.nvim_buf_set_lines(result_buffer, 0, 0, false, { self.results })
+      vim.api.nvim_buf_set_option(result_buffer, 'buftype', 'nofile')
+      vim.api.nvim_buf_set_option(result_buffer, 'swapfile', false)
+      vim.api.nvim_buf_set_option(result_buffer, 'buflisted', false)
+      vim.api.nvim_buf_set_option(result_buffer, 'filetype', 'json')
+
+      local query_buffer = vim.api.nvim_create_buf(false, false)
+      vim.api.nvim_buf_set_lines(query_buffer, 0, 0, false, { self.query })
+      vim.api.nvim_buf_set_option(query_buffer, 'buftype', 'nofile')
+      vim.api.nvim_buf_set_option(query_buffer, 'swapfile', false)
+      vim.api.nvim_buf_set_option(query_buffer, 'buflisted', false)
+      vim.api.nvim_buf_set_option(query_buffer, 'filetype', 'json')
+
       -- vim.api.nvim_buf_call(float_buffer, vim.cmd('%! jq .'))
       -- local float_win = vim.api.nvim_open_win(float_buffer, true, win_config)
+      -- vim.cmd('tabnew')
+      -- vim.api.nvim_win_set_buf(0, query_buffer)
+      -- vim.api.nvim_buf_call(query_buffer, vim.cmd('%! jq .'))
+
       vim.cmd('vsplit')
-      vim.api.nvim_win_set_buf(0, float_buffer)
-      vim.api.nvim_buf_call(float_buffer, vim.cmd('JqxList'))
+      vim.api.nvim_win_set_buf(0, result_buffer)
+      vim.api.nvim_buf_call(result, vim.cmd('%! jq .'))
     end,
   }
   vim.fn.jobstart(cmd, opts)
