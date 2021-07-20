4ce1c8198464bd59deb01c638faa8b9b156a18cc
otherJL0 <jonathanglopez@gmail.com>
2021-07-20 00:34:00 -0400

    Adding luacheck to efm

--- lua/config/lsp/efm/lua.lua
+++ lua/config/lsp/efm/lua.lua
@@ -1,1 +1,10 @@
-return {luaformat = {formatCommand = 'lua-format -i', formatStdin = true}}
+return {
+  luaformat = { formatCommand = 'lua-format -i', formatStdin = true },
+  luacheck = {
+    lintCommand = 'luacheck src -',
+    lintFormats = '%f:%l:%c: %trror: %m',
+    '%f:%l:%c: %tarning: %m',
+    '%f:%l:%c: %tote: %m',
+    lintSource = 'luacheck',
+  },
+}
