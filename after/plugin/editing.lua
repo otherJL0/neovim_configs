2a70a38b3621a8f15fdb93e6f8ad175ebb53d34d
otherJL0 <jonathanglopez@gmail.com>
2021-07-20 00:21:11 -0400

    Adding tab functionality

--- after/plugin/editing.lua
+++ after/plugin/editing.lua
@@ -14,21 +14,34 @@ require('nvim-autopairs.completion.compe').setup({
   map_complete = true, -- it will auto insert `(` after select function or method item
 })
 
+local function prequire(...)
+  local status, lib = pcall(require, ...)
+  if (status) then
+    return lib
+  end
+  return nil
+end
+
+local luasnip = prequire('luasnip')
+
 local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 local check_back_space = function()
   local col = vim.fn.col('.') - 1
-  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+    return true
+  else
+    return false
+  end
 end
 
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
 _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t '<C-n>'
+  elseif luasnip and luasnip.expand_or_jumpable() then
+    return t '<Plug>luasnip-expand-or-jump'
   elseif check_back_space() then
     return t '<Tab>'
   else
@@ -38,13 +51,10 @@ end
 _G.s_tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t '<C-p>'
+  elseif luasnip and luasnip.jumpable(-1) then
+    return t '<Plug>luasnip-jump-prev'
   else
-    -- If <S-Tab> is not working in your terminal, change it to <C-h>
     return t '<S-Tab>'
   end
 end
 
-vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
