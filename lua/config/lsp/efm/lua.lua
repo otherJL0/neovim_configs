7c842e54379ef5adb263df0be82975bfe6f5a3d5
otherJL0 <jonathanglopez@gmail.com>
2021-07-20 00:44:03 -0400

    Attempt to fix

--- lua/config/lsp/efm/lua.lua
+++ lua/config/lsp/efm/lua.lua
@@ -1,7 +1,7 @@
 return {
   luaformat = { formatCommand = 'lua-format -i', formatStdin = true },
   luacheck = {
-    lintCommand = 'luacheck src -',
+    lintCommand = 'luacheck -',
     lintFormats = '%f:%l:%c: %trror: %m',
     '%f:%l:%c: %tarning: %m',
     '%f:%l:%c: %tote: %m',
