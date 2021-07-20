6d5b63960bf5c71676cd815ad607614f6489d5e2
otherJL0 <jonathanglopez@gmail.com>
2021-07-20 00:15:20 -0400

    Adding tab completion

--- after/plugin/editing.lua
+++ after/plugin/editing.lua
@@ -1,6 +1,4 @@
 local npairs = require('nvim-autopairs')
-local ts_conds = require('nvim-autopairs.ts-conds')
-local inoremap = vim.keymap.inoremap
 
 require('nvim-ts-autotag').setup()
 require('nvim-treesitter.configs').setup { autopairs = { enable = true } }
@@ -15,3 +13,38 @@ require('nvim-autopairs.completion.compe').setup({
   map_cr = true, --  map <CR> on insert mode
   map_complete = true, -- it will auto insert `(` after select function or method item
 })
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+  local col = vim.fn.col('.') - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-n>'
+  elseif check_back_space() then
+    return t '<Tab>'
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t '<C-p>'
+  else
+    -- If <S-Tab> is not working in your terminal, change it to <C-h>
+    return t '<S-Tab>'
+  end
+end
+
+vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
